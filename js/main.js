b888ddaf465561ac36d8548aa1cd384098b73c96
JohniFi <25084862+JohniFi@users.noreply.github.com>
2022-07-11 16:03:28 +0200

    reorganize

--- js/main.js
+++ js/main.js
@@ -3,57 +3,12 @@ if (!window.FileReader) {
     //return;
 }
 
-// draw a data table
-function renderDatatable(resultset, tableRecordsEle) {
-    try {
-        tableRecordsEle.innerHTML = '';
-
-        let tableHtmlStr = '';
-        tableHtmlStr += '<table class="table table-striped table-condensed small table-bordered">';
-        tableHtmlStr += '<thead>';
-        tableHtmlStr += '<tr><th></th><th>' + resultset[0]['columns'].join('</th><th>') + '</th></tr>';
-        tableHtmlStr += '</thead>';
-        tableHtmlStr += '<tbody>';
-        let tableValues = resultset[0]['values'];
-        for (let v in tableValues) {
-            tableHtmlStr += '<tr><th>' + (parseInt(v) + 1) + '</th><td>' + tableValues[v].join('</td><td>') + '</td></tr>';
-        }
-        tableHtmlStr += '</tbody>';
-        tableHtmlStr += '</table>';
-        tableHtmlStr += '</div>';
-        tableRecordsEle.innerHTML = tableHtmlStr;
-
-        errorDisplay.innerHTML = '';
-
-        return;
-    } catch (err) {
-        throw new Error(err.message);
-    }
-}
-
-
-function readFileAsArrayBuffer(file) {
-    return new Promise((resolve, reject) => {
-        let fileredr = new FileReader();
-        fileredr.onload = () => resolve(fileredr.result);
-        fileredr.onerror = () => reject(fileredr);
-        fileredr.readAsArrayBuffer(file);
-    });
-}
-
-
-// ---- upload ----
-var openDatabase = document.getElementById('open_database');
+let errorDisplay = document.getElementById('errorDisplay');
+let openDatabase = document.getElementById('open_database');
+let tableRecords = document.getElementById('tableRecords');
 
 // the database
-var db = null;
-
-// SQL query
-var stmt = '';
-// SQL query result
-var resultset = [];
-
-var tableRecords = document.getElementById('tableRecords');
+let db = null;
 
 openDatabase.addEventListener('change', async (ev) => {
     errorDisplay.innerHTML = '';
@@ -69,8 +24,8 @@ openDatabase.addEventListener('change', async (ev) => {
 
 
         // render datatable records
-        stmt = 'SELECT * FROM sqlite_master';
-        resultset = db.exec(stmt);
+        let stmt = 'SELECT * FROM sqlite_master';
+        let resultset = db.exec(stmt);
         renderDatatable(resultset, tableRecords);
 
 
@@ -81,14 +36,72 @@ openDatabase.addEventListener('change', async (ev) => {
 }); // upload file change event
 
 
+function readFileAsArrayBuffer(file) {
+    return new Promise((resolve, reject) => {
+        let fileredr = new FileReader();
+        fileredr.onload = () => resolve(fileredr.result);
+        fileredr.onerror = () => reject(fileredr);
+        fileredr.readAsArrayBuffer(file);
+    });
+}
+
+
+// draw a data table
+function renderDatatable(resultset, tableRecordsEle) {
+    try {
+        tableRecordsEle.innerHTML = '';
+
+        let tableHtmlStr = '';
+        tableHtmlStr = `<table class="table table-striped table-condensed small table-bordered">
+                            <thead>
+                                <tr><th>${resultset[0]['columns'].join('</th><th>')}</th></tr>
+                            </thead>
+                            <tbody>`
+        let tableValues = resultset[0]['values'];
+        for (let v in tableValues) {
+            tableHtmlStr += `<tr><td>${tableValues[v].join('</td><td>')}</td></tr>`;
+        }
+        tableHtmlStr += '</tbody></table>';
+
+        tableRecordsEle.innerHTML = tableHtmlStr;
+
+        errorDisplay.innerHTML = '';
+
+        return;
+    } catch (err) {
+        errorDisplay.innerHTML = err.message;
+        return;
+    }
+}
+
+
+
+document.getElementById('list_mitarbeiter').addEventListener('click', listMitarbeiter)
+
+function listMitarbeiter() {
+    if (!db) {
+        errorDisplay.innerHTML = 'noch keine Datenbank geöffnet';
+    } else {
+        errorDisplay.innerHTML = '';
+        let stmt = 'SELECT * FROM mitarbeiter';
+        resultset = db.exec(stmt);
+        renderDatatable(resultset, tableRecords);
+    }
+}
+
+
+
 document.getElementById('import_html').addEventListener('change', importClinicPlanner)
 
 function importClinicPlanner() {
     let files = this.files;
+
     if (files.length === 0) {
-        console.log('No file is selected');
+        errorDisplay.innerHTML = 'keine Datei ausgewählt';
         return;
     }
+
+    errorDisplay.innerHTML = '';
 
     let reader = new FileReader();
     reader.readAsText(files[0], 'ISO-8859-1');
@@ -98,68 +111,64 @@ function importClinicPlanner() {
         let parser = new DOMParser();
         let clinicPlan = parser.parseFromString(event.target.result, "text/html");
 
+        try {
+            let clinicPlanTable = clinicPlan.getElementsByTagName('table')[0];
 
-        let table = clinicPlan.getElementsByTagName('table')[0];
+            // start reading on table row x
+            const CLINICPLAN_ROW_START = 2;
+            // skip every x lines
+            const CLINICPLAN_ROW_OFFSET = 4;
+            // start reading on table col x
+            const CLINICPLAN_COL_START = 4;
 
-        // start reading on table row x
-        const CLINICPLAN_ROW_START = 2;
-        // skip every x lines
-        const CLINICPLAN_ROW_OFFSET = 4;
-        // start reading on table col x
-        const CLINICPLAN_COL_START = 4;
+            let dienstplan = [];
 
-        let clinicPlanName = "";
-        let clinicPlanFirstname = "";
+            // get date from upper left corner of the table
+            let clinicPlanDate = clinicPlanTable.rows[0].cells[0].innerText.trim();
+            // format date to YYYY-MM-
+            clinicPlanDate = `${clinicPlanDate.slice(-4)}-${clinicPlanDate.substring(3, 5)}-`;
 
-        let dienstplan = [];
+            // console.log("Anz. Mitarbeiter: " + (table.rows.length-ClinicPlanRowStart)/ClinicPlanRowOffset);
 
-        // console.log("Anz. Mitarbeiter: " + (table.rows.length-ClinicPlanRowStart)/ClinicPlanRowOffset);
+            // iterate over staff
+            for (let i = CLINICPLAN_ROW_START, row; row = clinicPlanTable.rows[i]; i += CLINICPLAN_ROW_OFFSET) {
 
-        // get date from upper left corner of the table
-        let clinicPlanDate = table.rows[0].cells[0].innerText.trim();
-        // format date to YYYY-MM-
-        clinicPlanDate = clinicPlanDate.slice(-4) + "-" + clinicPlanDate.slice(3, 5) + "-";
+                let clinicPlanName = row.cells[0].innerText.trim(); // row = table.rows[i]
+                let clinicPlanFirstname = clinicPlanTable.rows[i + 1].cells[0].innerText.trim();
+                let clinicPlanPosition = clinicPlanTable.rows[i + 2].cells[0].innerText.trim().substring(0, 2); // OA, FA, As
 
+                //console.log(clinicPlanName + ", " + clinicPlanFirstname); // log processed name
+                for (let j = CLINICPLAN_COL_START, cell; cell = row.cells[j]; j++) {
 
-        let dienst = "";
-        let dienstIst = "";
+                    // get dienstIst from row below 
+                    let dienstIst = clinicPlanTable.rows[i + 1].cells[j].innerText.trim();
+                    // overwrite dienst to dienstIst if not empty
+                    let dienst = (dienstIst === "") ? cell.innerText.trim() : dienstIst;
 
-        // iterate over staff
-        for (let i = CLINICPLAN_ROW_START, row; row = table.rows[i]; i += CLINICPLAN_ROW_OFFSET) {
+                    if (!(dienst === "")) {
+                        // add entry to dienstplan
+                        dienstplan.push(
+                            {
+                                datum: clinicPlanDate + (`00${j + 1 - CLINICPLAN_COL_START}`).slice(-2),
+                                vorname: clinicPlanFirstname,
+                                nachname: clinicPlanName,
+                                dienst: dienst,
+                                position: clinicPlanPosition
+                            }
+                        );
 
-
-            clinicPlanName = row.cells[0].innerText.trim();
-            clinicPlanFirstname = table.rows[i + 1].cells[0].innerText.trim();
-
-            //console.log(clinicPlanName + ", " + clinicPlanFirstname); // log processed name
-
-
-            for (let j = CLINICPLAN_COL_START, cell; cell = row.cells[j]; j++) {
-
-                // get dienstIst from row below 
-                dienstIst = table.rows[i + 1].cells[j].innerText.trim();
-                // overwrite dienst to dienstIst if not empty
-                dienst = (dienstIst === "") ? cell.innerText.trim() : dienstIst;
-
-                if (!(dienst === "")) {
-                    dienstplan.push(
-                        {
-                            datum: clinicPlanDate + ("00" + (j + 1 - CLINICPLAN_COL_START)).slice(-2), // get two-digits with leading zero
-                            vorname: clinicPlanFirstname,
-                            nachname: clinicPlanName,
-                            dienst: dienst,
-                        }
-                    );
-
-                    console.log(dienstplan.slice(-1)[0]); // log last entry
+                        console.log(dienstplan.slice(-1)[0]); // log last entry
+                    }
                 }
+
+
             }
-
-
+        } catch (err) {
+            errorDisplay.innerHTML = `beim Laden des ClinicPlanners ist ein Fehler aufgetreten: ${err.message}`;
+            return;
         }
 
         // console.log("Anzahl Dienste: " + dienstplan.length) // number of entrys
-
     };
 
 }
